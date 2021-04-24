8688002c7083706d55c8ce6cb5a04e4d102a17c9
Kacper Chętkowski <kacper.chetkowski@gmail.com>
2021-04-24 12:14:10 +0200

    Added copper_ore in java

--- src/main/java/com/example/examplemod/ExampleModDefs.java
+++ src/main/java/com/example/examplemod/ExampleModDefs.java
@@ -1,8 +1,13 @@
 package com.example.examplemod;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -21,5 +26,24 @@ public class ExampleModDefs{
 	
 	public static final RegistryObject<Item> COPPER = ITEMS.register("copper",
 			() -> new Item(new Item.Properties().group(ItemGroup.MATERIALS))
+	);
+	
+	public static final RegistryObject<Block> COPPER_ORE = BLOCKS.register("copper_ore",
+			() -> new Block(
+					Block.Properties
+						.create(Material.IRON)
+						.hardnessAndResistance(5.0f, 6.0f)
+						.sound(SoundType.STONE)
+						.harvestTool(ToolType.PICKAXE)
+						.harvestLevel(1)
+						.setRequiresTool()
+			)
+	);
+	
+	public static final RegistryObject<Item> COPPER_ORE_ITEM = ITEMS.register("copper_ore",
+			() -> new BlockItem(
+					COPPER_ORE.get(),
+					new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)
+			)
 	);
 }
